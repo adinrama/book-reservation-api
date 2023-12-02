2e33bd33270afaf4764c8a4b5a82f596b55b707b
adinrama <adinrama@student.ub.ac.id>
2023-12-02 10:59:15 +0700

    add users and books data

---
+++ utils/books.js
@@ -0,0 +1,29 @@
+const books = [
+  {
+    title: "Design Patterns: Elements of Reusable Object-Oriented Software",
+    type: "technology",
+    author: "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides",
+    publisher: "Addison-Wesley Professional",
+    publish_year: "October 21, 1994",
+    page_counts: 416,
+  },
+  {
+    title: "Clean Coder Conduct Professional Programmers",
+    type: "technology",
+    author: "Robert Cecil Martin",
+    publisher: "Pearson: 1st edition",
+    publish_year: "May 13, 2011",
+    page_counts: 256,
+  },
+  {
+    title: "Introduction to Algorithms",
+    type: "technology",
+    author:
+      "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein",
+    publisher: "The MIT Press",
+    publish_year: "September 1, 2009",
+    page_counts: 1292,
+  },
+];
+
+module.exports = books;
---
+++ utils/users.js
@@ -0,0 +1,20 @@
+const users = [
+  {
+    name: "Al Fajri Yunus",
+    email: "fajri@gmail.com",
+    password: "fajri1234",
+    gender: "male",
+    roles: "user",
+    address: "Jl. Simpang Waringin No. 13A, Moyoketen",
+  },
+  {
+    name: "Bima Antasena",
+    email: "bima@gmail.com",
+    password: "bima1234",
+    gender: "male",
+    roles: "admin",
+    address: "Jl. Ahmad Dahlan, Kabupaten Sleman",
+  },
+];
+
+module.exports = users;
