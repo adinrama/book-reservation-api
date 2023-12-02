7c5cf3cda63d7febcc17edad29bc68f67d9e9d90
adinrama <adinrama@student.ub.ac.id>
2023-12-02 14:31:55 +0700

    add books data

--- utils/books.js
+++ utils/books.js
@@ -24,6 +24,22 @@ const books = [
     publish_year: "September 1, 2009",
     page_counts: 1292,
   },
+  {
+    title: "The Pragmatic Programmer",
+    type: "technology",
+    author: "Andrew Hunt, David Thomas",
+    publisher: "Addison-Wesley Professional",
+    publish_year: "September 13, 2019",
+    page_counts: 352,
+  },
+  {
+    title: "Refactoring: Improving the Design of Existing Code",
+    type: "technology",
+    author: "Martin Fowler",
+    publisher: "Addison-Wesley Professional",
+    publish_year: "November 30, 2018",
+    page_counts: 448,
+  },
 ];
 
 module.exports = books;
