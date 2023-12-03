5840358eae4c65b78229ecb1fda581e9fe52743c
adinrama <adinrama@student.ub.ac.id>
2023-12-03 21:37:37 +0700

    update login

--- handlers/users.js
+++ handlers/users.js
@@ -1,27 +1,15 @@
 const users = require("../utils/users");
-const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { SECRET_KEY } = process.env;
 
 const login = (request, h) => {
   const { email, password } = request.payload;
 
   for (i = 0; i < users.length; i++) {
     if (users[i].email == email && users[i].password == password) {
-      users[i].token = jwt.sign(
-        {
-          email: users[i].email,
-          roles: users[i].roles,
-        },
-        SECRET_KEY,
-        { expiresIn: "24h" }
-      );
-
       return h
         .response({
           message: "Login successfully",
           status: "Success",
-          token: users[i].token,
         })
         .code(200);
     }
