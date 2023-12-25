03ffaea4ecad7c293b74a60dc67f3920e7dbbe4a
adinrama <adinrama@student.ub.ac.id>
2023-12-25 10:12:31 +0700

    create documentation for books route

--- routes/books.js
+++ routes/books.js
@@ -1,3 +1,4 @@
+const Joi = require("joi");
 const {
   getAllBooks,
   getBookById,
@@ -10,27 +11,97 @@ const routes = [
   {
     method: "GET",
     path: "/v1/books",
-    handler: getAllBooks,
+    options: {
+      handler: getAllBooks,
+      description: "Endpoint for get all books",
+      notes: "Execute to see the result",
+      tags: ["api", "books"],
+    },
   },
   {
     method: "GET",
     path: "/v1/books/{id}",
-    handler: getBookById,
+    options: {
+      handler: getBookById,
+      description: "Endpoint for get book by id",
+      notes: "Execute to see the result",
+      tags: ["api", "books"],
+      validate: {
+        params: Joi.object({
+          id: Joi.number().required(),
+        }),
+      },
+    },
   },
   {
     method: "POST",
     path: "/v1/books",
-    handler: addNewBook,
+    options: {
+      handler: addNewBook,
+      description: "Endpoint for upload a new book",
+      notes:
+        "Enter the title, type, author, publisher, publish_year, and page_counts to upload a new book",
+      tags: ["api", "books"],
+      plugins: {
+        "hapi-swagger": {
+          payloadType: "form",
+        },
+      },
+      validate: {
+        payload: Joi.object({
+          title: Joi.string().required(),
+          type: Joi.string().required(),
+          author: Joi.string().required(),
+          publisher: Joi.string().required(),
+          publish_year: Joi.string().required(),
+          page_counts: Joi.number().required(),
+        }),
+      },
+    },
   },
   {
     method: "PUT",
     path: "/v1/books/{id}",
-    handler: updateBookById,
+    options: {
+      handler: updateBookById,
+      description: "Endpoint for update an existing book",
+      notes:
+        "Optional enter the title, type, author, publisher, publish_year, and page_counts to upload a new book",
+      tags: ["api", "books"],
+      plugins: {
+        "hapi-swagger": {
+          payloadType: "form",
+        },
+      },
+      validate: {
+        params: Joi.object({
+          id: Joi.number().required(),
+        }),
+        payload: Joi.object({
+          title: Joi.string(),
+          type: Joi.string(),
+          author: Joi.string(),
+          publisher: Joi.string(),
+          publish_year: Joi.string(),
+          page_counts: Joi.number(),
+        }),
+      },
+    },
   },
   {
     method: "DELETE",
     path: "/v1/books/{id}",
-    handler: deleteBookById,
+    options: {
+      handler: deleteBookById,
+      description: "Endpoint for delete book by id",
+      notes: "Execute to see the result",
+      tags: ["api", "books"],
+      validate: {
+        params: Joi.object({
+          id: Joi.number().required(),
+        }),
+      },
+    },
   },
 ];
 
