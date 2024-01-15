8982ac8559d1017a06384c7dbe7669c2fd93c2a6
adinrama <adinrama@student.ub.ac.id>
2024-01-15 10:57:49 +0700

    add users data

--- utils/users.js
+++ utils/users.js
@@ -1,6 +1,15 @@
 const users = [
   {
     id: 1,
+    name: "Bima Antasena",
+    email: "bima@gmail.com",
+    password: "bima1234",
+    gender: "male",
+    roles: "admin",
+    address: "Jl. Ahmad Dahlan, Kabupaten Sleman, Yogyakarta",
+  },
+  {
+    id: 2,
     name: "Al Fajri Yunus",
     email: "fajri@gmail.com",
     password: "fajri1234",
@@ -9,13 +18,31 @@ const users = [
     address: "Jl. Simpang Waringin No. 13A, Moyoketen",
   },
   {
-    id: 2,
-    name: "Bima Antasena",
-    email: "bima@gmail.com",
-    password: "bima1234",
+    id: 3,
+    name: "Rendra Atmajaya",
+    email: "rendra@gmail.com",
+    password: "rendra1234",
     gender: "male",
-    roles: "admin",
-    address: "Jl. Ahmad Dahlan, Kabupaten Sleman",
+    roles: "user",
+    address: "Jl. Bunga Coklat No. 16, Kota Malang",
+  },
+  {
+    id: 4,
+    name: "Alfi Putri Sena",
+    email: "alfi@gmail.com",
+    password: "alfi1234",
+    gender: "female",
+    roles: "user",
+    address: "Jl. Demuk No. 21, Panjerejo",
+  },
+  {
+    id: 5,
+    name: "Sindy Riska",
+    email: "sindy@gmail.com",
+    password: "sindy1234",
+    gender: "female",
+    roles: "user",
+    address: "Jl. Tarumanegara No. 12B, Kota Medan",
   },
 ];
 
